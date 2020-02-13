b933b81e5aa1e8ecf0920e3e98063d6b3658fb20
siyuan9826@gmail.com <coder@Coders-MacBook-Pro.local>
2020-02-13 10:59:40 +0800

    on-task-3

--- js/index.js
+++ js/index.js
@@ -38,5 +38,91 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let a1 = document.querySelectorAll("nav a")[0]
+a1.textContent = "Services";
+
+let a2 = document.querySelectorAll("nav a")[1]
+a2.textContent = "Product";
+
+let a3 = document.querySelectorAll("nav a")[2]
+a3.textContent = "Vision";
+
+let a4 = document.querySelectorAll("nav a")[3]
+a4.textContent = "Features";
+
+let a5 = document.querySelectorAll("nav a")[4]
+a5.textContent = "About";
+
+let a6 = document.querySelectorAll("nav a")[5]
+a6.textContent = "Contact";
+
+// const home = document.createElement('a');
+// home.textContent = "Home"; 
+
+// const newA = document.querySelectorAll('nav a');
+// newA.appendChild(home);
+
+//  document.querySelectorAll("nav a").style.backgroundColor = "green";
+
+
+
+document.getElementById("logo-img").src="img/logo.png";
+
+h1 = document.querySelector('h1');
+h1.textContent = 'DOM Is Awesome';
+
+button1 = document.querySelector('button');
+button1.textContent = 'Get Started';
+
+document.getElementById("cta-img").src="img/header-img.png";
+
+
+let h41 = document.querySelectorAll("h4")[0]
+h41.textContent = "Features";
+
+let h42 = document.querySelectorAll("h4")[1]
+h42.textContent = "About";
+
+let h43 = document.querySelectorAll("h4")[2]
+h43.textContent = "Services";
+
+let h44 = document.querySelectorAll("h4")[3]
+h44.textContent = "Product";
+
+let h45 = document.querySelectorAll("h4")[4]
+h45.textContent = "Vision";
+
+let p1 = document.querySelectorAll('.main-content p')[0]
+p1.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let p2 = document.querySelectorAll('.main-content p')[1]
+p2.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let p3 = document.querySelectorAll('.bottom-content p')[0]
+p3.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let p4 = document.querySelectorAll('.bottom-content p')[1]
+p4.textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let p5 = document.querySelectorAll('.bottom-content p')[2]
+p5.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+document.getElementById("middle-img").src="img/mid-page-accent.jpg";
+
+const h47 = document.querySelectorAll(' h4')[5]
+h47.textContent = "Contact";
+
+let p6 = document.querySelectorAll('.contact p')[0]
+p6.textContent = "123 Way 456 Street Somewhere, USA";
+
+let p7 = document.querySelectorAll('.contact p')[1]
+p7.textContent = "1 (888) 888-8888";
+
+let p8 = document.querySelectorAll('.contact p')[2]
+p8.textContent = "sales@greatidea.io";
+
+let footer1 = document.querySelectorAll('footer p')
+footer1.textContent = "Copyright Great Idea! 2018";
